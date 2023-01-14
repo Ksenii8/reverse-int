6b9ced5d1dbdfa9f8fdbacf0c2454ec2cae24f0d
Ksenii8 <119839152+Ksenii8@users.noreply.github.com>
2023-01-14 19:02:15 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+   var s = "";
+  for (var i = String(Math.abs(n)).length - 1; i>=0; i--) {
+    s +=String(Math.abs(n))[i];
+  }
+  return Number(s);
 }
